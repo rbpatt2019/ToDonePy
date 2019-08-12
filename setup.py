c4bdb92689a03c78903ba875c1dc2aa5197cef9e
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-08-12 15:18:17 -0400

    Initiate setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+        name='studious_happiness', 
+        version='0.0.1',
+        author='Ryan Patterson',
+        author_email='ryan.patterson.2015@gmail.com',
+        description='Keep calm and manage your tasks!',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/rbpatt2019/studious_happiness',
+        package_dir={'': 'src'},
+        packages=setuptools.find_packages('src'),
+        # entry_points={
+        #     'console_scripts':[
+        #         'command = cli.file:function']},
+        classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3.7'
+            ],
+        install_requires=['click'],
+        python_requires='>=3.7'
+        )
