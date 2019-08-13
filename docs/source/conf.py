d16909076c4f4816704aab5f72592632d6576220
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-08-13 13:23:55 -0400

    Added basic sphnix compacity.
    Note: index.rst still not configured

---
+++ docs/source/conf.py
@@ -0,0 +1,70 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'studious-happiness'
+copyright = '2019, Ryan B Patterson'
+author = 'Ryan B Patterson'
+
+# Short version (should work with bump2version)
+version = "0.0.3"
+# The full version, including alpha/beta/rc tags
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+        'sphinx.ext.autodoc',
+        'sphinx.ext.autosummary',
+        'sphinx.ext.doctest',
+        'sphinx.ext.githubpages',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'nature'
+html_theme_options = {
+        # 'description': 'Keep calm and manage your tasks!',
+        # 'page_width': '100%',
+        # 'github_button': 'true',
+        # 'github_banner': 'true',
+        # 'github_type': 'star',
+        # 'github_user': 'rbpatt2019',
+        # 'github_repo': 'studious-happiness',
+        }
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
