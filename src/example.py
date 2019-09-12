7a6e4ca7741079df27e459220972d0ffccd625be
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-12 12:53:43 -0400

    Add mypy typing to example

--- src/example.py
+++ src/example.py
@@ -7,7 +7,7 @@ import click
 @click.command()
 @click.option("--count", default=1, help="Number of greetings")
 @click.option("--name", prompt="Your name", help="The persion to greet.")
-def hello(count, name):
+def hello(count: int, name: str) -> NONE:
     """Greet the nice person NAME COUNT times
 
     :param count: Number of greetings
