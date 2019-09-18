f82427d5b8b559d1528263ea6266ae3900fbcec3
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-17 20:56:06 -0400

    Initiate to and do

--- src/command_line/ToDoneCLI.py
+++ src/command_line/ToDoneCLI.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import click
 from pathlib import Path
+from ToDonePy.filer import Filer as Filer
+import click
 
 
 @click.group()
+@click.option("--file", "-f", envvar='TODO_FILE', default=Path.home())
 @click.version_option(version="0.2.1")
-@click.option("--file", "-f", default=Path.home() / "TODO.csv")
-def to(file) -> None:
+@click.pass_context
+def to(ctx, file: Path) -> None:
     """Base command for managing tasks
 
     :param file: Location of TODO.csv. Defaults to $HOME/TODO.csv
+
     :note: If you use a location other than the default for --file, 
         I'd recommend an alias so as to avoid typing it every time
     """
-    click.echo(f"Hello {name}!")
+    ctx.obj = Filer(file / "TODO.csv", create=True)
 
 
 @to.command()
-def do():
+@click.argument('task')
+@click.argument('rank')
+@click.pass_obj
+def do(obj, task: str, rank: int) -> None:
     """Add a task to your list
 
-    :arg1: TODO
-    :returns: TODO
+    :param task: Task to be added to your list
+    :param rank: Priority to assign to this task
 
     """
+    obj.write('.'.join([str(rank), task]))
+    click.echo('successful test')
