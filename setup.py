abbb1376f153ac45d526e20a3c7e5bb6610937ef
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-18 10:33:29 -0400

    Update .travis.yml settings

--- setup.py
+++ setup.py
@@ -7,28 +7,27 @@ with open("README.rst") as f:
     long_description = f.read()
 
 setuptools.setup(
-        name='ToDonePy', 
-        version="0.2.1",
-        author='Ryan Patterson',
-        author_email='ryan.patterson.2015@gmail.com',
-        description='Keep calm and manage your tasks!',
-        long_description=long_description,
-        long_description_content_type='text/x-rst',
-        url='https://github.com/rbpatt2019/ToDonePy',
-        package_dir = {"": "src"},
-        packages = setuptools.find_packages('src'),
-        entry_points={
-            'console_scripts':[
-                'to = command_line.ToDoneCLI:to']
-        },
-        classifiers=[
-            'Development Status :: 2 - Pre-Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Natural Language :: English',
-            'Operating System :: Unix',
-            'Programming Language :: Python :: 3.7'
-            ],
-        install_requires=['click'],
-        python_requires='>=3.7'
-        )
+    name="ToDonePy",
+    version="0.2.1",
+    author="Ryan Patterson",
+    author_email="ryan.patterson.2015@gmail.com",
+    description="Keep calm and manage your tasks!",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/rbpatt2019/ToDonePy",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    entry_points={"console_scripts": ["to = command_line.ToDoneCLI:to"]},
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=["click"],
+    python_requires=">=3.5",
+)
