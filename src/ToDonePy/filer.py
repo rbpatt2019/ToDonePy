c92b57667e766354d11b1e6fa805de394d59661f
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-18 12:06:41 -0400

    Move whole command to Path type

--- src/ToDonePy/filer.py
+++ src/ToDonePy/filer.py
@@ -20,7 +20,7 @@ class Filer(object):
     
     """
 
-    def __init__(self, path: str, create: bool = True) -> None:
+    def __init__(self, path: Path, create: bool = True) -> None:
         """Initialise the Filer
 
         :path: A Unix filepath to the desired file
@@ -28,9 +28,9 @@ class Filer(object):
 
         """
         self.path = path
-        if not isfile(Path(self.path)):
+        if not isfile(self.path):
             if create:
-                Path(self.path).touch()
+                self.path.touch()
             else:
                 raise OSError("File does not exist")
 
--- src/command_line/ToDoneCLI.py
+++ src/command_line/ToDoneCLI.py
@@ -10,24 +10,28 @@ from ToDonePy.filer import Filer as Filer
 
 @click.group()
 @click.option(
-    "--file", "-f", envvar="TODO_FILE", default=(str(Path.home()) + "/TODO.csv")
+    "--file",
+    "-f",
+    envvar="TODO_FILE",
+    default=(Path.home() / "TODO.csv"),
+    type=click.Path(exists=False),
 )
 @click.version_option(version="0.2.1")
 @click.pass_context
-def to(ctx, file: str) -> None:
+def to(ctx, file: Path) -> None:
     """Base command for managing tasks
 
     :param file: Location of TODO.csv. Defaults to $HOME/TODO.csv
 
     :note: If you use a location other than the default for --file, 
-        I'd recommend an alias so as to avoid typing it every time
+        I'd recommend setting TODO_FILE as an environemtal variable
     """
     ctx.obj = Filer(file, create=True)
 
 
 @to.command()
-@click.argument("task")
-@click.argument("rank")
+@click.argument("task", required=True)
+@click.argument("rank", required=True)
 @click.pass_obj
 def do(obj, task: str, rank: int) -> None:
     """Add a task to your list
