1fc624e2aa20ba3047218753cc2e14fd85663729
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-19 15:13:13 -0400

    Update docstrings

--- tests/make_temp.py
+++ tests/make_temp.py
@@ -1,11 +1,22 @@
 from pathlib import Path
 
 def make_path(tmp_path: Path) -> Path:
-    """Helper function to create a pathlib Path object."""
+    """Helper function to create a pathlib Path object.
+    
+    :temp_path: Temporary path for pytest
+    :returns: Temporary file path for pytest
+    
+    """
     return (tmp_path / "TODO.csv")
 
 def make_file(tmp_path: Path, content: str = "") -> Path:
-    """Helper function to create a temporary file"""
+    """Helper function to create a temporary file
+
+    :temp_path: Temporary path for pytest
+    :contets: Contents to be written to temporary file
+    :returns: Temporary file for pytest
+
+    """
     csv = tmp_path / "TODO.csv"
     csv.write_text(content)
     return csv
