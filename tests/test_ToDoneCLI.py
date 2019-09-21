624cbee6ef86f4f0dd7b291fe21f7c16bcd6cb3b
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-20 20:59:53 -0400

    Update to do test with time stamp

--- tests/test_ToDoneCLI.py
+++ tests/test_ToDoneCLI.py
@@ -4,6 +4,7 @@
 from pathlib import Path
 
 from click.testing import CliRunner
+from datetime import datetime as dt
 
 from command_line.ToDoneCLI import to
 from tests.make_temp import make_file, make_path
@@ -13,11 +14,12 @@ def test_to_do_custom_file(tmp_path):
     """Run to do with existing custom file"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        csv = make_file(tmp_path, "1,Old task\n")
+        csv = make_file(tmp_path, "1,Old task,2019-09-20 20:56:00\n")
         result = runner.invoke(to, ["--file", f"{csv}", "do", "New task", "1"])
+        date = dt.now().strftime("%Y-%m-%d %H:%M:%S")
         assert result.exit_code == 0
         assert result.output == "Task added\n"
-        assert Path(csv).read_text() == "1,Old task\n1,New task\n"
+        assert Path(csv).read_text() == f"1,Old task,2019-09-20 20:56:00\n1,New task,{date}\n"
 
 def test_to_doing_custom_file(tmp_path):
     """Run to do with existing custom file"""
