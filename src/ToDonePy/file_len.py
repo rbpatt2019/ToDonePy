3b40ab80b5d939e783eefb8bed7a968a572ed91f
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-30 11:54:58 -0400

    Create file_len.py

---
+++ src/ToDonePy/file_len.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import subprocess
+
+def file_len(file: Path) -> int:
+    """Find the number of lines in a file
+
+    :file: Path to the file
+
+    :returns: Number of lines in a file
+    """
+    wc = subprocess.Popen(['wc', '-l', file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result, err = wc.communicate()
+    if wc.returncode != 0:
+        raise IOError(err)
+    return int(result.strip().split()[0])
