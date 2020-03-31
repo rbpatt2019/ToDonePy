d65aa3abfcea676603539999fb89d3bf87e2225d
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-03-31 06:40:02 -0400

    Formatting

--- src/command_line/ToDoneCLI.py
+++ src/command_line/ToDoneCLI.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Tuple
 
 import click
+
 from ToDonePy.counted_list import counted_list as counted_list
 from ToDonePy.file_len import file_len as file_len
 from ToDonePy.filer import Filer as Filer
