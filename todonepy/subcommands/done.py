2e8cb138fa393cc8d9ff8938ef00885ce4769070
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-07-05 10:08:25 -0400

    Refactor docstring for done

--- todonepy/subcommands/done.py
+++ todonepy/subcommands/done.py
@@ -1,16 +1,39 @@
+"""The `done` subcommand for the `to` main command"""
+import argparse
+
 from helpers.file_len import file_len
 
 
-def done(args) -> None:
+def done(args: argparse.Namespace) -> None:
     """Remove a task to your list
 
-    :tasks: Task(s) to be added to your list. Supports any number of arguments
+    This command uses the supplied tasks to look for (partial) matches in your TODO
+    list. A helpful message lets you know if the task(s) was(were) found and deleted.
+    
+    Notes
+    -----
+        If your task contains more than one word, then each task must be enclosed in
+        quotes. Otherwise, the CLI treats each word as a task. Also note that if
+        multiple lines match a task, they will ALL be deleted.
 
-    :Note: If multiple lines match an of the ``tasks``, they will all be deleted.
+    Parameters
+    ----------
+    args : argparse.Namespace
+        Arguments forwarded from the CLI. For this subcommand, this includes:
+        args.file : Filer
+            The TODO file to be searched
+        args.task : List[str]
+            The list of tasks to be deleted
+ 
+    Returns
+    -------
+    None
+        Though a message will be echoed letting you know if the task(s) was(were)
+        deleted successfully.
 
-    :Note: If your task is more than 1 word long, enclose it in quotes
-
-    :Note: If a task is not found, then the CLI will say so
+    Example
+    -------
+    >>> to done 'An example' 'Is always helpful'
 
     """
     for item in args.tasks:
