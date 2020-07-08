97268356a67cae5f9799f11080f43fa0e37f238c
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-07-08 08:02:19 -0400

    Files altered

--- tests/test_doing.py
+++ tests/test_doing.py
@@ -8,44 +8,45 @@ from todonepy import to
 from subcommands.doing import doing
 from helpers.filer import Filer
 
-expected_none = "ID\tRank\tDate\tTask\n1\t2\t2019-09-20 20:56\tOld task\n2\t1\t2019-09-24 12:57\tNew task\n"
-expected_both = "ID\tRank\tDate\tTask\n1\t1\t2019-09-24 12:57\tNew task\n2\t2\t2019-09-20 20:56\tOld task\n"
+expected_none = [
+    ["ID", "Rank", "Date", "Task"],
+    ["1", "2", "2019-09-20 20:56", "Old task"],
+    ["2", "1", "2019-09-24 12:57", "New task"],
+]
+expected_both = [
+    ["ID", "Rank", "Date", "Task"],
+    ["1", "1", "2019-09-24 12:57", "New task"],
+    ["2", "2", "2019-09-20 20:56", "Old task"],
+]
 
 
-@pytest.mark.parametrize('sort,expected', [('none', expected_none), ('both', expected_both)])
-def test_to_doing(sort: str, expected: str, tmp_file: Path, capsys):
+@pytest.mark.parametrize(
+    "sort,expected", [("none", expected_none), ("both", expected_both)]
+)
+def test_to_doing(sort: str, expected: str, tmp_file: Filer, capsys):
     """Run to doing with existing custom file
 
     Parametrised to test situations where `--sort` is/isn't passed"""
 
     to(
         argparse.Namespace(
-            func=doing,
-            file=Filer(tmp_file),
-            sort=sort,
-            number=5,
-            edit=False,
-            reminder=False,
+            func=doing, file=tmp_file, sort=sort, number=5, edit=False, reminder=False,
         )
     )
     out, err = capsys.readouterr()
 
-    assert tmp_file.read_text() == expected
+    assert tmp_file.read() == expected
 
     assert err == ""
-    assert out == expected
+    assert out == "\n".join(["\t".join(i) for i in expected]) + "\n"
 
-@pytest.mark.skip(reason='Vim hangs indefinitely')
-def test_to_doing_custom_file_edit_flag(tmp_file: Path, capsys):
+
+@pytest.mark.skip(reason="Vim hangs indefinitely")
+def test_to_doing_edit(tmp_file: Filer, capsys):
     """Run to doing with the edit flag"""
     to(
         argparse.Namespace(
-            func=doing,
-            file=Filer(tmp_file),
-            sort='none',
-            number=5,
-            edit=True,
-            reminder=False,
+            func=doing, file=tmp_file, sort="none", number=5, edit=True, reminder=False,
         )
     )
     out, err = capsys.readouterr()
@@ -54,17 +55,12 @@ def test_to_doing_custom_file_edit_flag(tmp_file: Path, capsys):
 
 
 @pytest.mark.skipif(which("notify-send") is None, reason="Requires notify-send")
-def test_to_doing_custom_file_graphic_flag(tmp_file, capsys):
+def test_to_doing_reminder(tmp_file, capsys):
     """Run to doing with the `--reminder` flag"""
 
     to(
         argparse.Namespace(
-            func=doing,
-            filer=Filer(tmp_file),
-            sort="none",
-            number=5,
-            edit=False,
-            reminder=True,
+            func=doing, file=tmp_file, sort="none", number=5, edit=False, reminder=True,
         )
     )
     out, err = capsys.readouterr()
